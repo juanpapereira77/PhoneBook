f90f88411b5a368667a014bff6b9228a1e8c0257
juanpablo <juanpablopr.15@gmail.com>
2019-08-31 13:23:46 -0400

    webapi

---
+++ MVC-phone/apiPhone/Models/Phone.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace apiPhone.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public enum TypeContact
+    {
+        Phonenumber,
+        Email,
+        Facebook,
+        Twitter,
+        LinkedIN
+
+    }
+
+    public class Phone
+    {
+        [key]
+
+        public int PhoneID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public TypeContact Type { get; set; }
+        [Required]
+        public string Contact { get; set; }
+    }
+
+}
