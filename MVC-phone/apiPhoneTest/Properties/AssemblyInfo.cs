1dffbe20a9ae761e656b8ee6782c63a9cd9b01e1
juanpablopr.15@gmail.com <juanpablopr.15@gmail.com>
2019-11-23 13:57:08 -0400

    test done

---
+++ MVC-phone/apiPhoneTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("apiPhoneTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("apiPhoneTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("a355729c-a744-4517-80ae-6fa99cf231bf")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MVC-phone/apiPhoneTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace apiPhoneTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestControllerExistePhoneExists()
+        {
+            //organizar
+            var controller = new apiPhone.Controllers.PhonesController ();
+            //ejecuto
+            bool reponse = controller.PhoneExists(1);
+            //actuar
+            Assert.AreEqual(false, reponse);
+
+
+        }
+    }
+}
